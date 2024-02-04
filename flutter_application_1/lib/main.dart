15811e698cdfb3a6ee483accb8773e7565b85f47
ShreyaaaJoshi <shreyajoshi1510@gmail.com>
2024-02-04 16:49:08 +0530

    first commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyQuizApp());
+}
+
+class MyQuizApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: QuizScreen(),
+    );
+  }
+}
+
+class QuizScreen extends StatefulWidget {
+  @override
+  _QuizScreenState createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  int _questionIndex = 0;
+  int _totalScore = 0;
+
+  final List<Map<String, Object>> _questions = [
+    {
+      'question': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 55},
+        {'text': 'Green', 'score': 30},
+        {'text': 'White', 'score': 17},
+      ],
+    },
+    {
+      'question': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
+    },
+    {
+      'question': 'Who is your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
+    },
+  ];
+
+  void _answerQuestion(int score) {
+    setState(() {
+      _totalScore += score;
+      _questionIndex++;
+    });
+  }
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Quiz App'),
+      ),
+      body: _questionIndex < _questions.length
+          ? Quiz(
+              question: _questions[_questionIndex]['question'].toString(),
+              answers: _questions[_questionIndex]['answers'] as List<Map<String, Object>>,
+              answerQuestion: _answerQuestion,
+            )
+          : Result(_totalScore, _resetQuiz),
+    );
+  }
+}
+
+class Quiz extends StatelessWidget {
+  final String question;
+  final List<Map<String, Object>> answers;
+  final Function(int) answerQuestion;
+
+  Quiz({
+    required this.question,
+    required this.answers,
+    required this.answerQuestion,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Question(question),
+        ...answers.map((answer) {
+          return Answer(
+            () => answerQuestion(answer['score'] as int),
+            answer['text'] as String,
+          );
+        }).toList(),
+      ],
+    );
+  }
+}
+
+class Question extends StatelessWidget {
+  final String questionText;
+
+  Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 20),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: ElevatedButton(
+        onPressed: () => selectHandler(),
+        child: Text(answerText),
+      ),
+    );
+  }
+}
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Your Score: $resultScore',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          ElevatedButton(
+            onPressed: () => resetHandler(),
+            child: Text('Restart Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
+}
